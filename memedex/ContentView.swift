631bed196c78f783c0fc6f319ea9a22453d5abb5
Solomon Demmessie <meagh054@fb.com>
2020-02-12 14:23:25 -0800

    Initial Commit

---
+++ memedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memedex
+//
+//  Created by meagh054 on 2/12/20.
+//  Copyright © 2020 solomon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
